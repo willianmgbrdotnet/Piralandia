b348a245920af84c9d8a925da184db8c4157089a
WillianMGBrdotnet <willianmgbr@hotmail.com>
2023-03-09 17:16:49 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+            string n = Console.ReadLine();
+            //continue a solução
+           
+            string v = new string(arr);
+            Console.WriteLine(v);
+
+    }
+
+}
